70b756a38b0f87aa43851cb978f882a3947ed57a
Tonyoh <gavrel.antonin@gmail.com>
2018-01-14 23:39:59 +0100

    [FIX] ft_swap

--- srcs/ft_swap.s
+++ srcs/ft_swap.s
@@ -3,11 +3,11 @@ global _ft_swap
 section .text
 
 _ft_swap:			    ; int ft_swap(int *a, int *b) : swaps two integers' value
-    test   ecx, edx     ; checks if eax (a) is equal to ebx (b)
+    test   edi, esi     ; checks if eax (a) is equal to ebx (b)
     je     .end         ; if so then we should avoid xoring a and b
-	xor    ecx, edx     ; *a = *a + *b
-    xor    edx, ecx     ; *b = *a - *b
-    xor    ecx, edx     ; *a = *a - *b
+	xor    edi, esi     ; *a = *a + *b
+    xor    esi, edi     ; *b = *a - *b
+    xor    edi, esi     ; *a = *a - *b
 
 .end
 	ret
