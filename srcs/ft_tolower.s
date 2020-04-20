65441fd1adb601c58b93b55b81201108c6f6a672
agavrel <gavrel.antonin@gmail.com>
2020-04-20 05:44:42 +0200

    [ADD] Tolower.s without look up table

--- srcs/ft_tolower.s
+++ srcs/ft_tolower.s
@@ -1,13 +1,12 @@
 global _ft_tolower
 
-extern table_type					; includes table_type
-
 section .text
 
 _ft_tolower:							; int	ft_tolower(int c);
-	lea     rcx, [rel table_type]		; Load Effective (memory) Address of table_type in rax
-	mov		eax, edi
-	or		eax, 0x20					; 0x20 : it sets the 6th bit which is the diff between lower and uppercase letters
-	test    byte[rcx + rdi], 0b00000001 ; rdi is int c, al contains 8 lowest bytes of rax, al = byte[&table_type + c]
-	cmovz	eax, edi					; set function's return value 'eax' equals to edi, which is int c passed, if it was an alpha
+	lea     edx, [edi - ('A')]	; we substract the value of the letter A
+	mov     eax, edi			; return value set to input value
+	or      edi, 0x20			; create a lowercase version
+	cmp     edx, 'Z'-'A'		; that we will use only if we were facing an upper case character
+	cmovb   eax, edi			; if it was, we move value from edi to eax
 	ret
+
